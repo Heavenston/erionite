3f2b927e0a834efe30c11691ad7ba0f506d8d442
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-04-09 10:04:21 +0200

    change(main): Reduced subdivs density

--- erionite/src/main.rs
+++ erionite/src/main.rs
@@ -99,8 +99,8 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut camera: ResMut<Cam>,
 ) {
-    let subdivs = 19u32;
-    let aabb_size = 2f64.powi(subdivs as i32);
+    let subdivs = 18u32;
+    let aabb_size = 2f64.powi((subdivs+1) as i32);
     let radius = aabb_size / 4.;
     let aabb: DAabb = DAabb::new_center_size(DVec3::ZERO, DVec3::splat(aabb_size));
 
