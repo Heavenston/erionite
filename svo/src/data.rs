f0a723a275dca086d0a05f4f155db516e1c9b9bd
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-04-15 16:57:09 +0200

    feat(svo): Impl Data for tuple of containing Datas

--- svo/src/data.rs
+++ svo/src/data.rs
@@ -16,18 +16,6 @@ pub type EitherDataRef<'a, D: Data> = Either<&'a D::Internal, &'a D>;
 #[allow(type_alias_bounds)]
 pub type EitherDataMut<'a, D: Data> = Either<& 'a mut D::Internal, & 'a mut D>;
 
-impl Data for () {
-    type Internal = ();
-}
-
-impl InternalData for () {  }
-
-impl SplittableData for () {
-    fn split(self) -> (Self::Internal, [Self; 8]) {
-        ((), [(); 8])
-    }
-}
-
 pub trait SplittableData: Data {
     fn split(self) -> (Self::Internal, [Self; 8]);
 }
@@ -54,3 +42,120 @@ impl<D: Data<Internal = ()>> AggregateData for D {
         _d: [EitherDataRef<D>; 8]
     ) -> () { }
 }
+
+impl Data for () {
+    type Internal = ();
+}
+
+impl InternalData for () {  }
+
+impl SplittableData for () {
+    fn split(self) -> (Self::Internal, [Self; 8]) {
+        ((), [(); 8])
+    }
+}
+
+macro_rules! impl_tuple {
+    (
+        $($num:tt => $name:tt),*$(,)?
+    ) => {
+        impl<$($name),*> Data for ($($name),*,)
+            where $($name: Data),*
+        {
+            type Internal = ($($name::Internal),*,);
+        }
+
+        impl<$($name),*> InternalData for ($($name),*,)
+            where $($name: InternalData),*
+        {  }
+
+        impl<$($name),*> SplittableData for ($($name),*,)
+            where $($name: SplittableData),*
+        {
+            fn split(self) -> (Self::Internal, [Self; 8]) {
+                use ::itertools::Itertools;
+
+                let vals = (
+                    $(self.$num.split()),*,
+                );
+
+                let ints = (
+                    $(vals.$num.1.into_iter().collect_tuple::<(
+                        $name, $name, $name, $name,
+                        $name, $name, $name, $name,
+                    )>().expect("array of 8 elements")),*,
+                );
+
+                (
+                    ($(vals.$num.0),*,),
+                    [
+                        ($(ints.$num.0),*,), ($(ints.$num.1),*,),
+                        ($(ints.$num.2),*,), ($(ints.$num.3),*,),
+                        ($(ints.$num.4),*,), ($(ints.$num.5),*,),
+                        ($(ints.$num.6),*,), ($(ints.$num.7),*,),
+                    ]
+                )
+            }
+        }
+
+        impl<$($name),*,> MergeableData for ($($name),*,)
+            where $($name: MergeableData),*
+        {
+            fn can_merge(
+                this: &Self::Internal,
+                children: [&Self; 8]
+            ) -> bool {
+                $(
+                    $name::can_merge(&this.$num, children.map(|x| &x.$num))
+                )&&*
+            }
+
+            fn merge(
+                this: Self::Internal,
+                children: [Self; 8]
+            ) -> Self {
+                use ::itertools::Itertools;
+
+                let children = children.into_iter().collect_tuple::<(
+                    Self, Self, Self, Self,
+                    Self, Self, Self, Self,
+                )>().expect("array of 8 elements");
+
+                let children = (
+                    $([
+                        children.0.$num, children.1.$num,
+                        children.2.$num, children.3.$num,
+                        children.4.$num, children.5.$num,
+                        children.6.$num, children.7.$num,
+                    ]),*,
+                );
+
+                ($($name::merge(this.$num, children.$num)),*,)
+            }
+        }
+
+        impl<$($name),*> AggregateData for ($($name),*,)
+            where $($name: AggregateData),*
+        {
+            fn aggregate<'a>(
+                children: [EitherDataRef<Self>; 8]
+            ) -> Self::Internal {
+                ($(
+                    $name::aggregate(children.map(|x| 
+                        x.map_left(|x| &x.$num)
+                         .map_right(|x| &x.$num)
+                    ))
+                ),*,)
+            }
+        }
+    };
+}
+
+impl_tuple!(0 => A);
+impl_tuple!(0 => A, 1 => B);
+impl_tuple!(0 => A, 1 => B, 2 => C);
+impl_tuple!(0 => A, 1 => B, 2 => C, 3 => D);
+impl_tuple!(0 => A, 1 => B, 2 => C, 3 => D, 4 => E);
+impl_tuple!(0 => A, 1 => B, 2 => C, 3 => D, 4 => E, 5 => F);
+impl_tuple!(0 => A, 1 => B, 2 => C, 3 => D, 4 => E, 5 => F, 6 => G);
+impl_tuple!(0 => A, 1 => B, 2 => C, 3 => D, 4 => E, 5 => F, 6 => G, 7 => H);
